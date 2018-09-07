0374ba00215c622f355a35e6d2c96bb1cf64b9b9
David Koh <david.f.koh@gmail.com>
2018-09-07 15:45:49 -0500

    Fix up a few bugs
    Should test my script before committing it!
    
    Also add a gitignore

--- generate_localhost_certs.sh
+++ generate_localhost_certs.sh
@@ -4,17 +4,15 @@ set -euo pipefail
 
 if [ $# -lt 2 ]; then
     cat <<EOF 1>&2
-    USAGE: $0 <ca cert> [<subject>]
+    USAGE: $0 <ca key> <ca cert> [<subject>]
     Note - don't include common name in the subject - it will be set to localhost
 EOF
     exit 1
 fi
 
-CACERT=$1
-shift
-SUBJECT="${1:-}"
-shift
-
+CA_KEY=$1
+CA_CERT=$2
+SUBJECT=${3:-}
 SUBJ_PARAM=""
 if [ ! -z "$SUBJECT" ]; then
     SUBJ_PARAM="-subj $SUBJECT"
@@ -22,4 +20,4 @@ fi
 
 
 openssl req -newkey rsa:2048 -sha256 -keyout server.key -nodes -subj "${SUBJECT}/CN=localhost" -out server.csr
-openssl x509 -req -extfile <(printf "subjectAltName=DNS:localhost") -sha256 -days 365 -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt
+openssl x509 -req -extfile <(printf "subjectAltName=DNS:localhost") -sha256 -days 365 -in server.csr -CA $CA_CERT -CAkey $CA_KEY -CAcreateserial -out server.crt
